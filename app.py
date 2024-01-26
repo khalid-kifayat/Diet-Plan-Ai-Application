fe3cd623be3fb601b8edfed14c0b7d08aae4ef82
khalid-kifayat <khalid.kifayat@gmail.com>
2024-01-26 22:11:35 +0500

    commit

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ load_dotenv()
 
 
 st.title("Personalized Diet and Workout Recommender:coffee:")
+
 st.markdown('<style>h1{color: orange; text-align: center;}</style>', unsafe_allow_html=True)
 st.subheader('Your Best Food and Exercise Advisor:spoon:', divider='rainbow')
+
 st.markdown('<style>h3{color: pink;  text-align: center;}</style>', unsafe_allow_html=True)
 st.caption('Python-Langchain Application created by Khalid kifayat :sunglasses:')
-st.markdown('<style>h3{color: pink;  text-align: right;}</style>', unsafe_allow_html=True)
+
+st.markdown('<style>h5{color: pink;  text-align: right;}</style>', unsafe_allow_html=True)
 
 hide_streamlit_style = """
             <style>
